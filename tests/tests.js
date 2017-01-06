b053cf61fd97c88217d883e7dda04b726cdd52aa
frank chukwu <frank chukwu>
2017-01-06 16:47:27 +0100

    change assert to should in my test case

--- tests/tests.js
+++ tests/tests.js
@@ -3,43 +3,43 @@
 'use strict'
 
 var chai = require('chai');
-var assert = chai.assert;
+var expect = chai.expect;
 
 var myApp = require('../app/library.js');
 
 describe("Test for proper arithmetic functionality", function() {
   it("should return 2 as average for 1, 2, 3", function() {
-    assert(myApp.computeAverage(1,2,3) == 2);
+    expect(myApp.computeAverage(1,2,3)).to.equal (2);
   })
   it("should return 5 as average for 3, 7, 5", function() {
-    assert(myApp.computeAverage(3, 7, 5) == 5);
+    expect(myApp.computeAverage(3, 7, 5)).to.equal (5);
   })
   it("should return 1 as average for 1, 1, 1", function (){
-    assert(myApp.computeAverage(1, 1, 1) == 1);
+    expect(myApp.computeAverage(1, 1, 1)).to.equal (1);
   })
   it("should return 2 as average for 2, 2, 2", function(){
-    assert(myApp.computeAverage(2, 2, 2) == 2);
+    expect(myApp.computeAverage(2, 2, 2)).to.equal (2);
   })
   it("should return 120 as factorial for 5", function() {
-    assert(myApp.computeFactorial(5) == 120);
+    expect(myApp.computeFactorial(5)).to.equal (120);
   })
   it("should return 6 as factorial for 3", function(){
-    assert(myApp.computeFactorial(3) == 6);
+    expect(myApp.computeFactorial(3)).to.equal (6);
   })
 })
 
 
 describe("Test for temperature conversion functionality", function() {
   it("should return X for Celcius value Y", function() {
-    assert(myApp.convertTempCtoF(40) == 104);
+    expect(myApp.convertTempCtoF(40)).to.equal (104);
   })
   it("should return X for Celcius value Y", function() {
-    assert(myApp.convertTempCtoF(60) == 140);
+    expect(myApp.convertTempCtoF(60)).to.equal (140);
   })
   it("should return Y for Fahrenheit value X", function() {
-    assert(myApp.convertTempFtoC(140) == 60);
+    expect(myApp.convertTempFtoC(140)).to.equal (60);
   })
   it("should return Y for Fahrenheit value X", function() {
-    assert(myApp.convertTempFtoC(104) == 40);
+    expect(myApp.convertTempFtoC(104)).to.equal (40);
   })
 })
